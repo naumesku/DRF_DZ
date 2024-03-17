ea768d6d86427a9a43b04b451e2c56099a5eee5c
naumesku <prp4@bk.ru>
2024-03-17 12:53:28 +0300

    add 26.2. Celery - upgrate f

--- users/tasks.py
+++ users/tasks.py
@@ -6,10 +6,6 @@ from users.models import User
 def check_active_users():
     """Меняет пользователям не заходившим на сайт более 30 дней поле is_active на False"""
 
-    # user = User.objects.get(id=5) # Получаем пользователя, которого хотим обновить
-    # user.last_login = timezone.now() - datetime.timedelta(days=30) # Устанавливаем новое значение для поля last_login
-    # user.save() # Сохраняем изменения
-
     User.objects.filter(
         is_active=True,
         last_login__lte=datetime.datetime.now() - datetime.timedelta(days=30)
